7d2ba94eddd5f2b6231dc962b2bbc2ef0a8706f5
joaogilo-hub <joao.gilo@edu.senai.br>
2026-08-04 15:28:26 -0300

    Finalização da aula 03

---
+++ Aula-03/Vetores/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       /* Vetores de matrizes 
+           Vetores => é uma estrutura de dados utilizados para armazenar varios valores de um mesmo tipo de uma unica variavel
+
+          sintaxe: tipo[] nome_vetor = new tipo[tamanho];
+          */
+
+          /* Iniciar um vetor - > lista de frutas */
+
+          string[] listaFrutas = {"Maça", "Manga", "Morango", "Melancia", "Melão"};
+
+          Console.WriteLine("Alterar o terceiro elemento da lista");
+          Console.WriteLine(listaFrutas[2]);
+
+          Console.WriteLine("Alterar o segundo elemento da lista: ");
+          listaFrutas[1] = "Banana";
+
+          for (int indice = 0; indice < listaFrutas.Count(); indice++)
+        {
+            Console.WriteLine(listaFrutas[indice]);
+        }
+        
+        /***************************************************************/
+        foreach (string fruta in listaFrutas)
+        {
+            Console.WriteLine(fruta);
+        }
+    }
+}
