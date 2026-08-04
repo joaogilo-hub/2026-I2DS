1d485076d5d3cfd5b708ed6f0fa517175e7ec908
joaogilo-hub <joao.gilo@edu.senai.br>
2026-08-04 17:06:13 -0300

    Exercicios

---
+++ exercicio/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int celsius;
+        Console.WriteLine("Digite a temperatura em Graus Celcius");
+        celsius = int.Parse(Console.ReadLine());
+
+        double fahrenheit = (celsius * 9 / 5 ) + 32;
+
+        Console.WriteLine("A temperatura em Fahrenheit é: " + fahrenheit);
+    }
+}
+
+
