1746fc637ff459d7e03aa7b9dcf76189ef307983
joaogilo-hub <joao.gilo@edu.senai.br>
2026-08-12 11:15:07 -0300

    Aula-06 colletions em #C

---
+++ Aula-06/Filas/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("=== TRABALHO COM FILAS ===");
+
+        // 1 - criar uma nova fila vazia 
+        Queue<string> filaBanco = new Queue<string>();
+
+        // 2 - Adicionar elementos na fila
+        filaBanco.Enqueue("Zuleika");        //0
+        filaBanco.Enqueue("Asdrubal");       //1
+        filaBanco.Enqueue("pafuncio");       //2
+        filaBanco.Enqueue("Epaminondas");    //3
+
+        // 3 - percorrer elementos  de uma fila
+        Console.WriteLine();
+        foreach (var pessoa in filaBanco);
+        {
+            Console.WriteLine(pessoa);
+        }
+    }
+}
